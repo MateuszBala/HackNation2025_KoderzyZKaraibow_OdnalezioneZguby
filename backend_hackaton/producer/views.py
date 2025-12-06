e2333f5daf80f608e1e457ec34a830de86a207a7
Sebastian Przystawski <sebastian.przystawski@gmail.com>
2025-12-06 17:48:31 +0100

    add backend template

---
+++ backend_hackaton/producer/views.py
@@ -0,0 +1,9 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+@csrf_exempt
+def index(request):
+    if request.method == "GET":
+        return HttpResponse("Static GET response from producer")
+    if request.method == "POST":
+        return HttpResponse("Static POST response from producer")
