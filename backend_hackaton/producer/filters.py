7029af14d7c24633c7e9ddbfd519454aa08f8cfe
Sebastian Przystawski <sebastian.przystawski@gmail.com>
2025-12-06 22:02:08 +0100

    add filters lib prototype

---
+++ backend_hackaton/producer/filters.py
@@ -0,0 +1,17 @@
+def filter_by_distinkt(records, distinkt):
+    return ""
+
+def filter_by_title(records, title):
+    return ""
+
+def filter_by_item_type(records, item_type):
+    return ""
+
+def filter_by_category(records, category):
+    return ""
+
+def filter_by_found_location(records, location):
+    return ""
+
+def filter_by_found_date(records, found_date):
+    return ""
